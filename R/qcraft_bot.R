bfabf69c8f26b1f1e9e8a165f6ff4b9e5c28885a
Reu-hub <opondoreuben94@gmail.com>
2025-04-08 17:17:41 +0300

    added echarts on main script

--- R/qcraft_bot.R
+++ R/qcraft_bot.R
@@ -3,6 +3,7 @@
 # load necessary libraries
 suppressPackageStartupMessages({
   library(tidyverse)
+  library(echarts4r)
 })
 # turn off warnins
 options(warn = -1)
@@ -254,6 +255,272 @@ bot_alternative_viz %>%
   )+
   guides(color = guide_legend(nrow = 1)) +
   geom_rug(sides = "b", aes(color = NULL), alpha = 0.5)
+
+# -------------------------------------------------------------------------
+# echarts
+# -------------------------------------------------------------------------
+# credit rating
+# Generate labels for all possible ratings
+rating_labels <- fima_cra_y_axis(y_values = 1:22)
+
+# Convert the mapping to JSON for all ratings
+rating_json <- jsonlite::toJSON(setNames(rating_labels, 1:22))
+
+# Create the chart with the focused y-axis range
+bot_alternative_viz %>% 
+  # filter(year > 2013) %>% 
+  select(c(year, credit_rating_number, group)) %>% 
+  mutate(
+    group = factor(x = group, levels = c("Baseline Scenario","Alternative Scenario"))
+  ) %>% 
+  dplyr::group_by(group) %>%
+  echarts4r::e_charts(year) %>%
+  echarts4r::e_line(credit_rating_number) %>%
+  echarts4r::e_x_axis(
+    name = "",
+    type = "category"
+  ) %>%
+  echarts4r::e_y_axis(
+    scale = FALSE,
+    # Let's use JavaScript formatter for dynamic min/max
+    min = "dataMin",  # Use data minimum
+    max = "dataMax",  # Use data maximum
+    minInterval = 1,  # Ensure integer steps
+    axisLabel = list(
+      formatter = htmlwidgets::JS(paste0("
+        function(value) {
+          var labels = ", rating_json, ";
+          return labels[Math.round(value)] || value;
+        }
+      "))
+    )
+  ) %>%
+  echarts4r::e_legend(
+    bottom = "0%",
+    orient = "horizontal",
+    x = "center",
+    padding = c(5, 10, 5, 10)
+  ) %>%
+  echarts4r::e_tooltip(
+    trigger = "axis",
+    formatter = htmlwidgets::JS(paste0("
+        function(params) {
+          var year = params[0].axisValue;
+          var result = year;
+          var labels = ", rating_json, ";
+          
+          params.forEach(function(param) {
+            var numValue = Number(param.value[1]);
+            var formattedValue = labels[Math.round(numValue)] || numValue.toFixed(3);
+            result += '<br/>' + param.marker + param.seriesName + ': ' + formattedValue;
+          });
+          return result;
+        }
+      ")),
+    axisPointer = list(
+      type = "cross"
+    )
+  ) %>%
+  echarts4r::e_grid(
+    containLabel = TRUE,
+    top = "5%",  
+    bottom = "7%",
+    left = "5%",
+    right = "5%"
+  ) %>%
+  e_toolbox_feature(feature = c("saveAsImage"))
+
+# Debt-GDP ratio, %
+bot_alternative_viz %>% 
+  select(c(year, gross_debt_pct_gdp, group)) %>% 
+  mutate(
+    group = factor(x = group, levels = c("Baseline Scenario","Alternative Scenario"))
+  ) %>% 
+  dplyr::group_by(group) %>%
+  echarts4r::e_charts(year) %>%
+  echarts4r::e_line(gross_debt_pct_gdp) %>%
+  echarts4r::e_x_axis(
+    name = "",
+    type = "category"
+  ) %>%
+  echarts4r::e_y_axis(
+    scale = TRUE
+  ) %>%
+  echarts4r::e_legend(
+    bottom = "0%",
+    orient = "horizontal",
+    x = "center",
+    padding = c(5, 10, 5, 10)
+  ) %>%
+  echarts4r::e_tooltip(
+    trigger = "axis",
+    formatter = htmlwidgets::JS("
+        function(params) {
+          var year = params[0].axisValue;
+          var result = year;
+          params.forEach(function(param) {
+            var value = Number(param.value[1]).toFixed(3);
+            result += '<br/>' + param.marker + param.seriesName + ': ' + value;
+          });
+          return result;
+        }
+      "),
+    axisPointer = list(
+      type = "cross"
+    )
+  ) %>%
+  echarts4r::e_grid(
+    containLabel = TRUE,
+    top = "5%",  
+    bottom = "7%",
+    left = "5%",
+    right = "5%"
+  ) %>%
+  e_toolbox_feature(feature = c("saveAsImage"))
+
+# Nominal GDP growth (%)
+bot_alternative_viz %>% 
+  select(c(year, gdp_growth_pct, group)) %>% 
+  mutate(
+    group = factor(x = group, levels = c("Baseline Scenario","Alternative Scenario"))
+  ) %>% 
+  dplyr::group_by(group) %>%
+  echarts4r::e_charts(year) %>%
+  echarts4r::e_line(gdp_growth_pct) %>%
+  echarts4r::e_x_axis(
+    name = "",
+    type = "category"
+  ) %>%
+  echarts4r::e_y_axis(
+    scale = TRUE
+  ) %>%
+  echarts4r::e_legend(
+    bottom = "0%",
+    orient = "horizontal",
+    x = "center",
+    padding = c(5, 10, 5, 10)
+  ) %>%
+  echarts4r::e_tooltip(
+    trigger = "axis",
+    formatter = htmlwidgets::JS("
+        function(params) {
+          var year = params[0].axisValue;
+          var result = year;
+          params.forEach(function(param) {
+            var value = Number(param.value[1]).toFixed(3);
+            result += '<br/>' + param.marker + param.seriesName + ': ' + value;
+          });
+          return result;
+        }
+      "),
+    axisPointer = list(
+      type = "cross"
+    )
+  ) %>%
+  echarts4r::e_grid(
+    containLabel = TRUE,
+    top = "5%",  
+    bottom = "7%",
+    left = "5%",
+    right = "5%"
+  ) %>%
+  e_toolbox_feature(feature = c("saveAsImage"))
+
+# Interest % Revenue
+bot_alternative_viz %>% 
+  select(c(year, interest_payments_pct_revenue, group)) %>% 
+  mutate(
+    group = factor(x = group, levels = c("Baseline Scenario","Alternative Scenario"))
+  ) %>% 
+  dplyr::group_by(group) %>%
+  echarts4r::e_charts(year) %>%
+  echarts4r::e_line(interest_payments_pct_revenue) %>%
+  echarts4r::e_x_axis(
+    name = "",
+    type = "category"
+  ) %>%
+  echarts4r::e_y_axis(
+    scale = TRUE
+  ) %>%
+  echarts4r::e_legend(
+    bottom = "0%",
+    orient = "horizontal",
+    x = "center",
+    padding = c(5, 10, 5, 10)
+  ) %>%
+  echarts4r::e_tooltip(
+    trigger = "axis",
+    formatter = htmlwidgets::JS("
+        function(params) {
+          var year = params[0].axisValue;
+          var result = year;
+          params.forEach(function(param) {
+            var value = Number(param.value[1]).toFixed(3);
+            result += '<br/>' + param.marker + param.seriesName + ': ' + value;
+          });
+          return result;
+        }
+      "),
+    axisPointer = list(
+      type = "cross"
+    )
+  ) %>%
+  echarts4r::e_grid(
+    containLabel = TRUE,
+    top = "5%",  
+    bottom = "7%",
+    left = "5%",
+    right = "5%"
+  ) %>%
+  e_toolbox_feature(feature = c("saveAsImage"))
+
+# Primary Balance, % of Nominal GDP
+bot_alternative_viz %>% 
+  select(c(year, primary_net_lending_pct_gdp, group)) %>% 
+  mutate(
+    group = factor(x = group, levels = c("Baseline Scenario","Alternative Scenario"))
+  ) %>% 
+  dplyr::group_by(group) %>%
+  echarts4r::e_charts(year) %>%
+  echarts4r::e_line(primary_net_lending_pct_gdp) %>%
+  echarts4r::e_x_axis(
+    name = "",
+    type = "category"
+  ) %>%
+  echarts4r::e_y_axis(
+    scale = TRUE
+  ) %>%
+  echarts4r::e_legend(
+    bottom = "0%",
+    orient = "horizontal",
+    x = "center",
+    padding = c(5, 10, 5, 10)
+  ) %>%
+  echarts4r::e_tooltip(
+    trigger = "axis",
+    formatter = htmlwidgets::JS("
+        function(params) {
+          var year = params[0].axisValue;
+          var result = year;
+          params.forEach(function(param) {
+            var value = Number(param.value[1]).toFixed(3);
+            result += '<br/>' + param.marker + param.seriesName + ': ' + value;
+          });
+          return result;
+        }
+      "),
+    axisPointer = list(
+      type = "cross"
+    )
+  ) %>%
+  echarts4r::e_grid(
+    containLabel = TRUE,
+    top = "5%",  
+    bottom = "7%",
+    left = "5%",
+    right = "5%"
+  ) %>%
+  e_toolbox_feature(feature = c("saveAsImage"))
 
 # end: --------------------------------------------------------------------
 
