c5224c4227eb525dede787e6636855a98aeefb20
Reu-hub <opondoreuben94@gmail.com>
2025-04-10 16:45:10 +0300

    improved the footer

--- components/ui/ui_footer_component.R
+++ components/ui/ui_footer_component.R
@@ -5,29 +5,24 @@ ui_footer_component <- function() {
     tags$div(
       class = "footer-content",
       tags$a(
-        class = "footer-link",
+        class = "footer-link logo-link",
         href = "https://www.ssdh.net/",
         target = "_blank",
         tags$img(
           src = "ssdh_logo.svg",
-          class = "header-logo",
-          style = "border-right: 2px solid white; padding-right: 10px; height: 50px"
+          class = "header-logo"
         )
       ),
       tags$a(
-        class = "footer-link",
-        # style = "border-right: 2px solid white; padding-right: 10px; height: 70px",
+        class = "footer-link text-link",
         href = "#",
         "Teal Insights"
       ),
       span(
         class = "footer-divider",
-        style = "padding-right: 10px;height : auto",
-        # HTML("&nbsp;|&nbsp;©&nbsp;"),
         lubridate::year(Sys.Date())
       )
     )
   )
 }
-
 # ends: -------------------------------------------------------------------
